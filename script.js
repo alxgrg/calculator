2e2ff64fd567d8fae02e55b7662f479afaed83c3
alxgrg <alxmgrg@gmail.com>
2022-02-15 22:30:44 -0500

    Add operator functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operate(operator, num1, num2) {
+  operator(num1, num2);
+}
